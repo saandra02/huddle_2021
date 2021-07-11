54d1d0786a3a2429945bd52e0655454bb95bade0
Saandra <saandra02@gmail.com>
2021-07-11 22:40:44 +0530

    Changed socket context from localhost to Heroku

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+const socket = io('https://demo-app-xyz.herokuapp.com/');
 
 const ContextProvider = ({ children }) => {
   
